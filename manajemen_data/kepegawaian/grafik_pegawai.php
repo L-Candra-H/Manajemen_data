a828c9a8303af996d123a76d464b64b46214da7a
L. Candra H. <35895992+L-Candra-H@users.noreply.github.com>
2026-08-11 22:05:36 +0700

    Update grafik_pegawai.php

--- manajemen_data/kepegawaian/grafik_pegawai.php
+++ manajemen_data/kepegawaian/grafik_pegawai.php
@@ -8,8 +8,8 @@ if (!isset($_SESSION['user_login'])) {
     exit;
 }
 
-error_reporting(1);
-ini_set('display_errors', 1);
+error_reporting(0);
+ini_set('display_errors', 0);
 
 $role = $_POST['role'] ?? '';
 $usere = $_POST['usere'] ?? '';
